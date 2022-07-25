79353d1aaefba4e4497de4d66356dd5cd92e9a61
Renan Souza <renantrsouza@gmail.com>
2022-07-25 11:24:55 -0400

    Create: base config

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-zinc-300 min-h-screen w-full flex justify-center items-center">
+      <strong>Hello World</strong>
+    </div>
+  )
+}
+
+export default App
