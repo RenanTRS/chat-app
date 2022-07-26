f3eb0a1c0b0ef2b8299184d3615706492e649e8d
Renan Souza <renantrsouza@gmail.com>
2022-07-26 13:17:31 -0400

    Style: add display flex

--- web/src/components/Messages/index.tsx
+++ web/src/components/Messages/index.tsx
@@ -16,8 +16,8 @@ export const Messages = ({ messages, user }: MessagesProps) => {
   }, [messages])
 
   return (
-    <main className="h-[calc(100vh-6.5rem)] relative">
-      <div className="overflow-x-scroll w-full max-h-[calc(100vh-6.5rem)] absolute bottom-0">
+    <main className="h-[calc(100vh-7rem)] relative">
+      <div className="overflow-y-scroll w-full max-h-[calc(100vh-6.5rem)] absolute bottom-0">
         <ul className="px-3 pt-4 flex flex-col gap-4 min-h-[2.5rem] w-full">
           {messages.map((message, key) => {
             return (
--- web/src/pages/Chat.tsx
+++ web/src/pages/Chat.tsx
@@ -16,10 +16,10 @@ export const Chat = () => {
   }
 
   return (
-    <>
+    <div className="flex flex-col">
       <Header user={'Another User'} />
       <Messages messages={messages} user={user} />
       <InputArea sendMessage={sendMessage} user={user} />
-    </>
+    </div>
   )
 }
