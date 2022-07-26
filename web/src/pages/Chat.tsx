71882694ae0917530ade24cc5dc25a749432a485
Renan Souza <renantrsouza@gmail.com>
2022-07-26 11:18:05 -0400

    Update: add message component

--- web/src/pages/Chat.tsx
+++ web/src/pages/Chat.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import { Header } from '../components/Header'
 import { InputArea } from '../components/InputArea'
+import { Messages } from '../components/Messages'
 
 export const Chat = () => {
-  const sendMessage = (value: string) => {
-    console.log(value)
+  const user = 'Renan'
+  type messagesType = {
+    user: string
+    message: string
   }
+  const [messages, setMessages] = useState<messagesType[]>([])
+
+  const sendMessage = ({ user, message }: messagesType) => {
+    setMessages([...messages, { user, message }])
+  }
+
   return (
     <>
       <Header user={'Another User'} />
-      <InputArea sendMessage={sendMessage} />
+      <Messages messages={messages} user={user} />
+      <InputArea sendMessage={sendMessage} user={user} />
     </>
   )
 }
