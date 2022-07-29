6b6898d846d09033d06f8b119ec17613493b5b4d
Renan Souza <renantrsouza@gmail.com>
2022-07-29 12:15:18 -0400

    perf style: Remove overflow scroll

--- web/src/components/Messages/index.tsx
+++ web/src/components/Messages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import { Message } from './Message'
 
 interface MessagesProps {
   messages: {
@@ -16,29 +17,12 @@ export const Messages = ({ messages, user }: MessagesProps) => {
   }, [messages])
 
   return (
-    <>
-      <main className="overflow-y-scroll h-full flex-grow">
-        <div className=" w-full h-full">
-          <ul className="px-3 pt-4 flex flex-col gap-4 min-h-[2.5rem] w-full">
-            {messages.map((message, key) => {
-              return (
-                <li
-                  key={key}
-                  className={`${
-                    message.user === user
-                      ? 'bg-green-500 rounded-t-xl rounded-l-xl self-end'
-                      : 'bg-zinc-200 rounded-t-xl rounded-r-xl self-start'
-                  } max-w-[11.25rem] py-3 px-2 text-base, leading-tight break-words`}
-                >
-                  {message.message}
-                </li>
-              )
-            })}
+    <ul className="px-3 pt-16 pb-14 w-full h-full flex flex-col gap-4">
+      {messages.map((message, key) => {
+        return <Message key={key} message={message} user={user} />
+      })}
 
-            <div ref={messageRef} />
-          </ul>
-        </div>
-      </main>
-    </>
+      <div ref={messageRef} />
+    </ul>
   )
 }
