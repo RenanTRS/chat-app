0a31081311961e0f790993f0b46e3b4e03121888
Renan Souza <renantrsouza@gmail.com>
2022-08-08 17:53:46 -0400

    style: add responsive to tablet and desktop

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import logoHomeImg from '../assets/logo-Home.svg'
+
 export const Home = () => {
   const [user, setUser] = useState<string>('')
   const navigate = useNavigate()
@@ -15,30 +17,33 @@ export const Home = () => {
 
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center gap-5 p-2">
-      <strong className="text-white text-5xl">Chat-App</strong>
+      <div className="flex items-center justify-center w-full">
+        <img src={logoHomeImg} alt="Logo" className="" />
+        <strong className="text-white text-5xl">Chat-App</strong>
+      </div>
 
       <form
         onSubmit={handlerSubmitLogin}
-        className="flex flex-col gap-3 w-full items-center"
+        className="flex gap-3 items-center justify-center w-auto md:w-[25rem]"
       >
-        <div className="text-xl text-white flex gap-3 items-center">
+        <div className="w-full md:w-[25rem] flex flex-col gap-4 text-white">
           <input
             type="text"
             value={user}
             onChange={(event) => setUser(event.target.value)}
-            placeholder="Usuário"
-            className="border border-green-500 bg-transparent rounded-full px-4 py-2 flex-1 w-full"
+            placeholder="User"
+            className="border border-green-500 bg-transparent rounded-full px-4 py-2 h-12 md:text-xl"
             required
           />
-        </div>
 
-        <button
-          type="submit"
-          className="bg-green-500 rounded-full p-3 w-1/2 disabled:opacity-50"
-          disabled={user.length === 0 || user.trim() === ''}
-        >
-          Entrar
-        </button>
+          <button
+            type="submit"
+            className="bg-green-500 rounded-full p-3 disabled:opacity-50 font-bold h-12"
+            disabled={user.length === 0 || user.trim() === ''}
+          >
+            Entrar
+          </button>
+        </div>
       </form>
     </div>
   )
